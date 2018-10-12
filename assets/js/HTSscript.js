439cb50b48c1d10bf5b0428071a13eff03d305e4
dkr <tnsc@hotmail.com>
2018-10-12 19:05:20 -0400

    better changes

--- assets/js/HTSscript.js
+++ assets/js/HTSscript.js
@@ -49,26 +49,3 @@ signbutton.addEventListener("click", scrollToSign);
 workbutton.addEventListener("click", scrollToWork);
 unibutton.addEventListener("click", scrollToUniform);
 orderbutton.addEventListener("click", scrollToOrder);
-
-// const url = './process.php';
-// const form = document.querySelector('form');
-
-// form.addEventListener('submit', e => {
-//     e.preventDefault();
-
-//     const files = document.querySelector('[type=file]').files;
-//     const formData = new FormData();
-
-//     for (let i = 0; i < files.length; i++) {
-//         let file = files[i];
-
-//         formData.append('files[]', file);
-//     }
-
-//     fetch(url, {
-//         method: 'POST',
-//         body: formData
-//     }).then(response => {
-//         console.log(response);
-//     });
-// });
